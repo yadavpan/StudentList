bc39d9231e337f87f94d2c8c1654c16e4eb35388
yadavpan <yadavpan@sheridancollege.ca>
2023-05-31 15:51:07 -0400

    Revert "Update StudentList.java"
    This reverts commit 9af080cb0fdfaef225796fd97bb29301f12720e2.

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -16,7 +16,6 @@ public class StudentList {
     public static void main(String[] args) {
         // TODO code application logic here
         // heavy hitter Pankaj Yadav
-        // this is my third comment
     }
     
 }
